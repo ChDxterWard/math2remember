49ca9e35ccaa684de6c409549c4dcd37fb91324e
Your Name <you@example.com>
2025-11-08 20:36:19 +0100

    Add extendedEuclidian and getPrimeFactors with some NN stuff (tbc).

--- math2remember/__init__.py
+++ math2remember/__init__.py
@@ -1,6 +1,15 @@
 from .isPrime import isPrime
 from .getPrimeGen import getPrimeGenFrom, getPrimeGenTo
 from .extendedEuclidian import extendedEuclidian
+from .fibGen import fibGen
+from .getPrimeFactors import getPrimeFactors
 
-__version__ = '0.1.10'
-__all__ = ['extendedEuclidian', 'isPrime', 'getPrimeGenFrom', 'getPrimeGenTo']
+__version__ = '0.1.11'
+__all__ = [
+        'extendedEuclidian', 
+        'isPrime', 
+        'getPrimeGenFrom', 
+        'getPrimeGenTo', 
+        'fibGen',
+        'getPrimeFactors'
+    ]
--- test/test_extendedEuclidian.py
+++ test/test_extendedEuclidian.py
@@ -7,15 +7,10 @@ def testExtendedEuclidian():
     assert gcd == 2
     assert x == 0
     assert y == 1
-    gcd, x, y = extendedEuclidian(173,11)
-    assert gcd == 1
-    gcd, x, y = extendedEuclidian(64,128)
-    assert gcd == 64
+    gcd, x, y = extendedEuclidian(15,45)
+    assert gcd == 15
     assert x == 1
     assert y == 0
-    gcd, x, y = extendedEuclidian(24,412)
-    assert gcd == 4
-    assert x == -17
-    assert y == 1
 
 
+                                        
