4de76fe2521fc4eb6405b98541b0b2335ee0bd94
Your Name <you@example.com>
2025-11-29 21:53:42 +0100

    Up sieve.

--- math2remember/getPrimeGen.py
+++ math2remember/getPrimeGen.py
@@ -5,23 +5,49 @@ def getPrimeGenFrom(n):
         if isPrime(n):
             yield n
         n += 1
-      
-def sieve(n):
-    l = [{'num':i,'isPrime': False} for i in range(0, n + 1)]
-    for ele in l:
-        if ele['isPrime']:
-            continue
-        if isPrime(ele['num']):
-                ele['isPrime']=True
-                idx = l.index(ele) * 2
-                while idx < n + 1:
-                    l[idx]['isPrime']=False    
-                    idx += ele['num']
-                    
-    return [ele['num'] for ele in l if ele['isPrime']] 
+
+def sieve(ceil):
+    if ceil < 2:
+        return []
+    # Create an array from 0 to ceil+1. Init all with true.
+    maybePrimes = [True] * (ceil+1)
+    # 0, 1 are not prime.
+    maybePrimes[0] = maybePrimes[1] = False
+    # 2 is the first prime number.
+    prime = 2
+    
+    # We take m with m <= ceil.
+    # We write m=a*b<=ceil with a <= b.
+    # Since a<=b <=> a*a<=a*b=m <=> a^2<=m<=ceil <=> a<=ceil**.5.
+    # So every m has an factor a with a<=ceil**.5. 
+    # a has an primefactor q with q<=a<=ceil**.5. We must have 
+    # discovered q and its multiplies allready because q<=a<=ceil**.5.
+    # So we discovered q and its multiples that are a and therefore m.
+    # If all primes <= ceil**.5 are discovered all combined numbers 
+    while prime <= ceil**.5:
+    #while prime*prime <= ceil:
+        # If maybePrimes[prime] is true it was not marked as an multiply
+        # of another number -> prime!
+        if maybePrimes[prime]:
+            # Now we know the current prime number.
+            # In the next step we have to mark the multiplies of that number 
+            # as false since these multiplies cant be primes.
+            # To do so we start with prime^2 add prime in each step to
+            # the current counter. With every step wir mark the current number 
+            # as not prime until we reach the end.
+            # We start with prime^2 beacause every multiple of prime < prime^2 
+            # was already visitied. E.g.: prime is 5. 5*5 = 25. 
+            # We look on 1*5,2*5,3*5,4*5. We see that the factors (1,2,3,4) 
+            # are all smaller then 5. So we surely visited them earlier when 
+            # we marked the multiples of 2 (and therefore 4) and 3.
+            for multiple in range(prime*prime,ceil+1,prime):
+                maybePrimes[multiple] = False
+        prime += 1
         
+    return [idx for idx in range(2, ceil+1) if maybePrimes[idx]]
+
 def getPrimeGenTo(n):
     return sieve(n)
 
 if __name__ == '__main__':
-    print(isPrime(1))
+    print(sieve(14))
--- test/test_getPrime.py
+++ test/test_getPrime.py
@@ -1,20 +1,24 @@
 import sys
 sys.path.append('../math2remember')
-from math2remember.isPrime import isPrime
-from math2remember.getPrimeGen import getPrimeGenFrom, getPrimeGenTo
+from math2remember.getPrimeGen import sieve
 
-def testGetPrimeGenFrom():
-    g = getPrimeGenFrom(-2)
-    assert next(g) == 2
-    assert next(g) == 3
-    assert next(g) == 5
-    assert next(g) == 7
+# def testGetPrimeGenFrom():
+#     g = getPrimeGenFrom(-2)
+#     assert next(g) == 2
+#     assert next(g) == 3
+#     assert next(g) == 5
+#     assert next(g) == 7
     
 def testGetPrimeGenTo():
-    assert [2]==getPrimeGenTo(2)
-    assert [2,3]==getPrimeGenTo(4)
-    assert [2,3, 5]==getPrimeGenTo(5)
-    assert [2,3, 5]==getPrimeGenTo(6)
-    assert [2,3,5,7]==getPrimeGenTo(7)
+    # assert [2]==getPrimeGenTo(2)
+    # assert [2,3]==getPrimeGenTo(4)
+    # assert [2,3, 5]==getPrimeGenTo(5)
+    # assert [2,3, 5]==getPrimeGenTo(6)
+    # assert [2,3,5,7]==getPrimeGenTo(7)
+    assert [2]==sieve(2)
+    assert [2,3]==sieve(4)
+    assert [2,3, 5]==sieve(5)
+    assert [2,3, 5]==sieve(6)
+    assert [2,3,5,7]==sieve(7)
 
 
